65d765a760debee2ca4afbc293ef6fa2936428a4
OsvaldoTA1 <osvaldotalavera22@gmail.com>
2024-12-09 03:26:07 -0500

    added icons folder in assets, set up components, finished product card, set up firebase authentication, added .env.example

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import "../styles/Home.css";
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
         <div>
-            <h1>Welcome To The Home Page!</h1>
-            <p>Discover the latest community events and updates here!</p>
+            <header className="hero">
+                <div className="hero-content">
+                    <h1>Message</h1>
+                    <div className="cta-button">
+                        <Link to="/events" className="btn btn-event">Explore Events</Link>
+                        <Link to="/marketplace" className="btn btn-marketplace">Visit Marketplace</Link>
+                    </div>
+                </div>
+            </header>
+
+            <div className="featured-events">
+
+            </div>
+
+            <div className="popular-products">
+
+            </div>
         </div>
     )
 };
