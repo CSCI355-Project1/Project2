2ac26aa0939b77e569b07e387b6f3b184b1d4afb
Luis Rodriguez <106412395+Luis-Rodriguez-02@users.noreply.github.com>
2024-12-11 15:54:50 -0500

    add product page and gitignore

---
+++ src/pages/Products.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Products = () => {
+    return (
+        <div>
+            <h1>Products</h1>
+            <p>Buy and sell products within your community!</p>
+        </div>
+    );
+};
+
+export default Products;
